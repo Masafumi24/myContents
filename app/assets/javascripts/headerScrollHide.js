86287adc0ee7d622248263d829f8df610ce1d8d0
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-08-05 14:16:35 +0900

    header JS

---
+++ app/assets/javascripts/headerScrollHide.js
@@ -0,0 +1,20 @@
+var _window = $(window),
+    _header = $('.header'),
+    heroBottom,
+    startPos,
+    winScrollTop;
+ 
+_window.on('scroll',function(){
+    winScrollTop = $(this).scrollTop();
+    heroBottom = $('.g').height();
+    if (winScrollTop >= startPos) {
+        if(winScrollTop >= heroBottom){
+            _header.addClass('hide');
+        }
+    } else {
+        _header.removeClass('hide');
+    }
+    startPos = winScrollTop;
+});
+ 
+_window.trigger('scroll');
