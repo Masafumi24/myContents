79bf4eaea6fc167b77ce9293d45ccf0cb66902d6
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-08-06 15:10:28 +0900

    index Top Image + header OK

--- app/assets/javascripts/headerScrollHide.js
+++ app/assets/javascripts/headerScrollHide.js
@@ -1,20 +1,22 @@
-var _window = $(window),
-    _header = $('.header'),
-    heroBottom,
-    startPos,
-    winScrollTop;
- 
-_window.on('scroll',function(){
-    winScrollTop = $(this).scrollTop();
-    heroBottom = $('.g').height();
-    if (winScrollTop >= startPos) {
-        if(winScrollTop >= heroBottom){
-            _header.addClass('hide');
+$(function () {
+    var _window = $(window),
+        header = $('.header'),
+        heroBottom,
+        startPos,
+        winScrollTop;
+    
+    _window.on('scroll',function(){
+        winScrollTop = $(this).scrollTop();
+        heroBottom = $('.g').height();
+        if (winScrollTop >= startPos) {
+            if(winScrollTop >= heroBottom){
+                header.addClass('hide');
+            }
+        } else {
+            header.removeClass('hide');
         }
-    } else {
-        _header.removeClass('hide');
-    }
-    startPos = winScrollTop;
+        startPos = winScrollTop;
+    });
+    
+    _window.trigger('scroll');
 });
- 
-_window.trigger('scroll');
