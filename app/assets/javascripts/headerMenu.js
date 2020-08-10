e4af6f580a8fbaeff05cb3342326efb6a8192175
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-08-10 12:48:40 +0900

    left drop menu OK

--- app/assets/javascripts/headerMenu.js
+++ app/assets/javascripts/headerMenu.js
@@ -1,7 +1,10 @@
 $(function () {
-
   $('.headerLeft .fa-bars').click(function(){
+    $('.drop').slideDown();
     $('.dropLeft').slideDown();
+    $('.dropLeft .fa-times').click(function(){
+      $('.drop').slideUp();
+      $('.dropLeft').slideUp();
+    })
   })
-
 });
