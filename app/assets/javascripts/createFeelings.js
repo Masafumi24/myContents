5a7ec403c8261cb890026479291917dfb142a582
Masafumi24 <61171572+Masafumi24@users.noreply.github.com>
2020-08-25 12:49:40 +0900

    Update createFeelings.js

--- app/assets/javascripts/createFeelings.js
+++ app/assets/javascripts/createFeelings.js
@@ -1,10 +1,8 @@
 $(function(){
   $('#new_feelings').on('submit', function(e){
     e.preventDefault();
-
     var formData = new FormData(this);
     var url = $(this).attr('action')
-
     $.ajax({
       url: url,
       type: "POST",
@@ -13,6 +11,5 @@ $(function(){
       processData: false,
       contentType: false
     })
-    
   })
 })
